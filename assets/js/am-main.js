21ec468a91127823c3e58c1a64474e1e13071711
dev-alamin <hmalaminmb4@gmail.com>
2023-01-24 21:51:45 +0600

    After finishing chi-siamo page (html)

--- assets/js/am-main.js
+++ assets/js/am-main.js
@@ -10,4 +10,35 @@ jQuery(document).ready(function ($) {
 
         })
     });
+
+    // chi siam bottom slider 
+    $('.valori-sliders').slick({
+        infinite: true,
+        slidesToShow: 4,
+        autoplay: true,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-long-arrow-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-long-arrow-right' aria-hidden='true'></i></button>",
+        nav: false,
+
+    });
+
+    $('.valori-sliders').on('afterChange', function (event, slick, currentSlide) {
+        var testing = $(".slick-active").children().first();
+
+        // var testing = $(".slick-active").children().first().addClass('active');
+        // var testing = currentSlide.children().first().addClass('active');
+
+        console.log(currentSlide);
+
+    });
+    //set active class to first slide
+    $('.slider-nav .slick-slide').eq(0).addClass('slick-active-awesome');
+
+
+    // $('.valori-sliders').on('afterChange', function () {
+    //     var dataId = $('.slick-current');
+    //     // console.log(dataId.children().first().addClass('active'));
+    //     console.log(dataId.children().addClass('active'));
+
+    // });
 });
